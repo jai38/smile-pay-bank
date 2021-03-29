9f7d49be55bbfec1752a2c6e801ec95ea4239ee0
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2021-03-30 01:48:17 +0530

    frontend update done

--- clientJS/Admin/updatesignup.js
+++ clientJS/Admin/updatesignup.js
@@ -1,13 +1,23 @@
+// let user = localStorage.getItem("currentUpdate");
+// user=JSON.parse(user)
+
+// changeDisplay= () => {
+//     document.getElementById("customerID").style.display="block";
+//     document.getElementById("customerID1").style.display="none";
+//     document.getElementById("customerID").innerHTML=user.customerID;
+// }
+
 let user = localStorage.getItem("currentUpdate");
 user=JSON.parse(user)
 console.log(user.name)
-document.getElementById("customerID").value=user.customerID;
-document.getElementById("account").value=user.account;
-document.getElementById("gender").value=user.gender;
-document.getElementById("DOB").value=user.DOB;
-document.getElementById("email").value=user.email;
-document.getElementById("pan").value=user.pan;
-document.getElementById("totalAmount").value=user.balance;
-document.getElementById("aadhar").value=user.aadhar;
-document.getElementById("name").value=user.name;
+document.getElementById("customerID1").innerHTML=user.customerID;
+document.getElementById("account1").innerHTML=user.account;
+document.getElementById("gender1").innerHTML=user.gender;
+document.getElementById("DOB1").innerHTML=user.DOB;
+document.getElementById("email1").innerHTML=user.email;
+document.getElementById("pan1").innerHTML=user.pan;
+document.getElementById("totalAmount1").innerHTML=user.balance;
+document.getElementById("aadhar1").innerHTML=user.aadhar;
+document.getElementById("name1").innerHTML=user.name;
 // document.getElementById("name").value=user.name;
+
--- routes/Admin/updateUser.js
+++ routes/Admin/updateUser.js
@@ -6,4 +6,6 @@ router.get("/", (req, res) => {
   res.render("./Admin/updateUser");
 });
 
+
+
 module.exports = router;
