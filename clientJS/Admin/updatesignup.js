d51da6b2b7657b967bcf3d7fdcdfdc7274470785
Jai Malani <71269258+jai38@users.noreply.github.com>
2021-03-31 20:22:04 +0530

    date type modified and css file created for updateSignup

--- clientJS/Admin/updatesignup.js
+++ clientJS/Admin/updatesignup.js
@@ -8,27 +8,27 @@
 // }
 
 let user = localStorage.getItem("currentUpdate");
-user=JSON.parse(user)
-let date=user.DOB;
-user.DOB=new Date(user.DOB).toLocaleDateString()
-console.log(user.name)
-document.getElementById("customerID1").innerHTML=user.customerID;
-document.getElementById("account1").innerHTML=user.account;
-document.getElementById("gender1").innerHTML=user.gender;
-document.getElementById("DOB1").innerHTML=user.DOB;
-document.getElementById("email1").innerHTML=user.email;
-document.getElementById("pan1").innerHTML=user.pan;
-document.getElementById("totalAmount1").innerHTML=user.balance;
-document.getElementById("aadhar1").innerHTML=user.aadhar;
-document.getElementById("name1").innerHTML=user.name;
-document.getElementById("customerID").value=user.customerID;
-document.getElementById("account").value=user.account;
-document.getElementById("pan").value=user.pan;
-document.getElementById("name").value=user.name;
-document.getElementById("gender").value=user.gender;
-document.getElementById("DOB").value=user.DOB;
-document.getElementById("email").value=user.email;
-document.getElementById("aadhar").value=user.aadhar;
-document.getElementById("totalAmount").value=user.balance;
+user = JSON.parse(user);
+let date = user.DOB;
+user.DOB = new Date(user.DOB).toISOString().substring(0, 10);
+// console.log(user.name);
+document.getElementById("customerID1").innerHTML = user.customerID;
+document.getElementById("account1").innerHTML = user.account;
+document.getElementById("gender1").innerHTML = user.gender;
+document.getElementById("DOB1").innerHTML = user.DOB;
+document.getElementById("email1").innerHTML = user.email;
+document.getElementById("pan1").innerHTML = user.pan;
+document.getElementById("totalAmount1").innerHTML = user.balance;
+document.getElementById("aadhar1").innerHTML = user.aadhar;
+document.getElementById("name1").innerHTML = user.name;
+document.getElementById("customerID").value = user.customerID;
+document.getElementById("account").value = user.account;
+document.getElementById("pan").value = user.pan;
+document.getElementById("name").value = user.name;
+document.getElementById("gender").value = user.gender;
+// console.log(user.DOB);
+document.getElementById("DOB").value = user.DOB;
+document.getElementById("email").value = user.email;
+document.getElementById("aadhar").value = user.aadhar;
+document.getElementById("totalAmount").value = user.balance;
 // document.getElementById("name").value=user.name;
-
